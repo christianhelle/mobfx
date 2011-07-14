08f56e24538e42575654705ac90c21a5c6379bd2
christianhelle_cp <SND\christianhelle_cp>
2011-07-14 13:26:52 +0000

    Added decorator pattern base class

---
+++ Mobile Framework/MobFx/Patterns/Decorator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristianHelle.Framework.WindowsMobile.Patterns
+{
+    /// <summary>
+    /// Base class for implementing the Decorator pattern
+    /// </summary>
+    /// <typeparam name="T">Object to decorate</typeparam>
+    public abstract class Decorator<T> : IDisposable
+    {
+        /// <summary>
+        /// The decorated object
+        /// </summary>
+        protected T Target;
+
+        /// <summary>
+        /// Creates an instance of <see cref="Decorator"/>
+        /// </summary>
+        /// <param name="target">Object to decorate</param>
+        protected Decorator(T target)
+        {
+            this.Target = target;
+        }
+
+        #region IDisposable Members
+
+        /// <summary>
+        /// Checks if the decorated object implements IDiposable, if so then Dispose() is called
+        /// </summary>
+        public void Dispose()
+        {
+            var disposable = Target as IDisposable;
+            if (disposable != null)
+                disposable.Dispose();
+        }
+
+        #endregion
+    }
+}
